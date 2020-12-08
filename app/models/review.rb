ca5d094a88c1ae4766a0c6262c2527d18d2afc4c
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-08 04:54:25 -0500

    fix review update

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,9 @@ class Review < ApplicationRecord
   validates_presence_of :rating
   belongs_to :user
   belongs_to :business
-  after_save :change_rating
+  after_create :change_rating
   before_destroy :delete_rating
+  around_update :update_rating
 
   private
 
@@ -21,6 +22,19 @@ class Review < ApplicationRecord
     total_reviews = Review.where("business_id = #{self.business_id}").size
     total = current_rating * total_reviews
     updated_rating = (total - self.rating) / (total_reviews - 1)
+    Business.find_by_id(self.business_id).update(rating: updated_rating)
+  end
+
+  def update_rating
+    current_rating = Business.find_by_id(self.business_id).rating
+    total_reviews = Review.where("business_id = #{self.business_id}").size
+    total = current_rating * total_reviews
+    newTotal = total - self.rating_was
+    # binding.pry
+    yield
+    newTotal += self.rating
+    updated_rating = newTotal / total_reviews
+    # binding.pry
     Business.find_by_id(self.business_id).update(rating: updated_rating)
   end
 end
